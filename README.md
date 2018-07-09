ab690caade2bf4f2cad7b2ea860572897216fd5e
aronsonben <14benaronson@gmail.com>
2018-07-09 17:46:51 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>README</h1>
+This project is a starting point for learning and practicing various front-end technologies, mainly React.js.
+
+Gratefully using <a href="https://github.com/ahfarmer/emoji-search">ahfarmer's 'emoji-search'</a> React example as a guideline to start.
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Header.css";
+
+class Header extends React.Component {
+    render() {
+        return(
+            <header className="component-header">
+                Frontend
+            </header>
+        );
+    }
+}
+export default Header;
