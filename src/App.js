782e7aa73483386d8bfe4e311988303502ce9c70
aronsonben <14benaronson@gmail.com>
2018-07-10 16:43:15 -0400

    added body

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from "./Header";
+import Body from "./Body";
 
 // Ben Aronson - change for github?
 class App extends React.Component {
@@ -7,6 +8,7 @@ class App extends React.Component {
         return (
             <div>
                 <Header />
+                <Body />
             </div>
         );
     }
---
+++ src/Body.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Body.css";
+
+class Body extends React.Component {
+    render() {
+        return(
+            <div className="component-body">
+                <h1>React-body</h1>
+                <p>The home page of React practice.</p>
+            </div>
+        );
+    }
+}
+export default Body;
