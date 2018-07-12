fceb9164765bbc6cfd5932f66d4b8f4c22331081
aronsonben <14benaronson@gmail.com>
2018-07-12 17:58:58 -0400

    trying different to-do list tutorial; see TodoList.js

--- src/Body.js
+++ src/Body.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import TodoForm from './TodoForm';
+import TodoList from './TodoList';
 import "./Body.css";
 
 class Body extends React.Component {
@@ -8,7 +8,7 @@ class Body extends React.Component {
             <div className="component-body">
                 <h1>React-body</h1>
                 <p>The home page of React practice.</p>
-                <TodoForm />
+                <TodoList />
             </div>
         );
     }
